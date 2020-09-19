764bf3e4061d6c2b3bd86c51935a9ac6206944b4
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-19 01:36:44 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,4 @@
 require 'pry'
-
 class Artist
   attr_accessor :name
   attr_reader :songs
@@ -14,7 +13,7 @@ class Artist
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
-
+  
   def self.all
     @@artists
   end
